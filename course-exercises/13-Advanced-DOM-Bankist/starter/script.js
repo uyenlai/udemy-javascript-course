b1d4f527a826fd271fffbb2b5f70d24599452fee
uyenlai <98347695+uyenlai@users.noreply.github.com>
2023-11-12 17:12:45 +0200

    implement tabbed component

--- course-exercises/13-Advanced-DOM-Bankist/starter/script.js
+++ course-exercises/13-Advanced-DOM-Bankist/starter/script.js
@@ -79,10 +79,39 @@ document.querySelector('.nav__links').addEventListener('click', e => {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
-///////////////////////////////////////
-///////////////////////////////////////
-///////////////////////////////////////
 
+//Tabbed Component
+const tabsContainer = document.querySelector('.operations__tab-container')
+const tabs = document.querySelectorAll('.operations__tab')
+const tabsContent = document.querySelectorAll('.operations__content')
+
+tabsContainer.addEventListener('click', (e) => {
+  const clicked = e.target.closest('.operations__tab')
+  console.log(clicked);
+
+  //Method 1
+  // if (clicked) { //clicked will return either null or the matched element from closest(), null is falsy value
+  //   clicked.classList.add('operations__tab--active')
+  // }
+
+  //Method 2
+  if(!clicked) return; //if clicked is null then return and the function stops operating
+  
+  //Before adding the active class to any element, first remove active class from all elements
+  tabs.forEach(t => t.classList.remove('operations__tab--active'))
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'))
+  //Activate tab
+  clicked.classList.add('operations__tab--active')
+
+  //Activate content
+  console.log(clicked.dataset.tab);
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+})
+
+
+///////////////////////////////////////
+///////////////////////////////////////
+//LECTURES
 /*
 ///////////////////////////////////////
 // Selecting, Creating, and Deleting Elements
@@ -200,5 +229,38 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 document.querySelector('.nav').addEventListener('click', function (e) {
   this.style.backgroundColor = randomColor();
   console.log('NAV', e.target, e.currentTarget);
+});
+*/
+
+///////////////////////////////////////
+// DOM Traversing
+/*
+const h1 = document.querySelector('h1');
+
+// Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);//this works only for direct children element
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
 });
 */
