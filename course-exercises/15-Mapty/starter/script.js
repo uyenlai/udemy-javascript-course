cd2b7fd4ae7373b9a1a1f11f53a2c4bd923c7327
uyenlai <98347695+uyenlai@users.noreply.github.com>
2023-11-19 11:17:38 +0200

    display a map marker

--- course-exercises/15-Mapty/starter/script.js
+++ course-exercises/15-Mapty/starter/script.js
@@ -19,7 +19,7 @@ if (navigator.geolocation) {
       const { longitude } = position.coords;
       console.log(longitude);
       console.log(`https://www.google.com/maps/@${latitude}.${longitude}`);
-      const coords = [latitude, longitude]
+      const coords = [latitude, longitude];
       const map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
@@ -27,12 +27,24 @@ if (navigator.geolocation) {
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
-
     function () {
       alert("Can't get your current position");
     }
