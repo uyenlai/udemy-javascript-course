204786599611397308934a8e57c6247aa958067d
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-05-09 17:55:30 +0200

    constructor function and new operator

---
+++ oop/index.js
@@ -0,0 +1,20 @@
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+//1. New {} is created
+//2. function is called, this = {}
+//3. {} is linked to prototype
+//4. function automatically return {}
+
+const uyen = new Person("Uyen", 2000);
+console.log(uyen);
+
+const chinh = new Person("Chinh", 2001);
+const dung = new Person("Dung", 2008);
