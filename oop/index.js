8d8ed23acd02cbd9852f4ae613d47b8593d1ed9f
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-05-10 22:17:12 +0200

    how to use a constructor

--- oop/index.js
+++ oop/index.js
@@ -25,6 +25,37 @@ Person.prototype.calcAge = function () {
 };
 
 console.log(uyen.__proto__);
-uyen.calcAge()
-chinh.calcAge()
-dung.calcAge()
+uyen.calcAge();
+chinh.calcAge();
+dung.calcAge();
+
+//A constructor of creating ducks (566/704)
+
+const Duck = function (type, canFly) {
+  this.type = type;
+  this.canFly = canFly;
+};
+
+const vittroi = new Duck("vittroi", "no");
+console.log(vittroi);
+
+// (567/704)
+const Dog = function (name, breed, weight) {
+  this.name = name;
+  this.breed = breed;
+  this.weight = weight;
+};
+
+const fluffy = new Dog("Fluffy", "Poodle", 30);
+const fido = new Dog("Fido", "Mixed", 38);
+const spot = new Dog("Spot", "Chihuahua", 10);
+
+const dogs = [fluffy, fido, spot];
+
+for (let i = 0; i < dogs.length; i++) {
+  var size = "small";
+  if (dogs[i].weight > 10) {
+    size = "large";
+  }
+  console.log("Dog: " + dogs[i].name + " is a " + size + " " + dogs[i].breed);
+}
