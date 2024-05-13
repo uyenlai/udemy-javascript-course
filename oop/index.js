693aad204838f9312a93d2ecd16f0733c183289d
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-05-13 16:29:26 +0200

    exercise for instanceof

--- oop/index.js
+++ oop/index.js
@@ -1,3 +1,4 @@
+/*
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
@@ -195,4 +196,44 @@ function Car(params) {
       alert("You need to start the engine first.");
     }
   };
+}
+*/
+
+// Understanding Object Instances (584/704)
+function dogCatcher(obj) {
+  if (obj instanceof Dog) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function Cat(name, breed, weight) {
+  this.name = name;
+  this.breed = breed;
+  this.weight = weight;
+}
+var meow = new Cat("Meow", "Siamese", 10);
+var whiskers = new Cat("Whiskers", "Mixed", 12);
+var fido = { name: "Fido", breed: "Mixed", weight: 38 };
+
+function Dog(name, breed, weight) {
+  this.name = name;
+  this.breed = breed;
+  this.weight = weight;
+  this.bark = function () {
+    if (this.weight > 25) {
+      alert(this.name + " says Woof!");
+    } else {
+      alert(this.name + " says Yip!");
+    }
+  };
+}
+var fluffy = new Dog("Fluffy", "Poodle", 30);
+var spot = new Dog("Spot", "Chihuahua", 10);
+var dogs = [meow, whiskers, fido, fluffy, spot];
+for (var i = 0; i < dogs.length; i++) {
+  if (dogCatcher(dogs[i])) {
+    console.log(dogs[i].name + " is a dog!");
+  }
 }
