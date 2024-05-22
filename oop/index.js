a986797e1d5e1bbe15818a344a858f828bbde5bb
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-05-22 15:13:50 +0200

    javascript oop finished

--- oop/index.js
+++ oop/index.js
@@ -261,7 +261,6 @@ var Dog = function (name, breed, weight) {
   this.weight = weight;
 };
 
-
 //this is a dog instance
 var spot = new Dog("Spot", "Chihuahua", 10);
 console.log(spot);
@@ -483,3 +482,26 @@ scotty.bark();
 beatrice.bark();
 scotty.gait("Walk");
 beatrice.groom();
+
+String.prototype.cliche = function () {
+  var cliche = ["lock and load", "touch base", "open the kimono"];
+  for (var i = 0; i < cliche.length; i++) {
+    var index = this.indexOf(cliche[i]);
+    if (index >= 0) {
+      return true;
+    }
+  }
+  return false;
+};
+
+var sentences = [
+  "I'll send my car around to pick you up.",
+  "Let's touch base in the morning and see where we are",
+  "We don't want to open the kimono, we just want to inform them.",
+];
+for (var i = 0; i < sentences.length; i++) {
+  var phrase = sentences[i];
+  if (phrase.cliche()) {
+    console.log("CLICHE ALERT: " + phrase);
+  }
+}
