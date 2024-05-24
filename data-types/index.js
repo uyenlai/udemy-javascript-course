d7ec04a9087946eb539d391237fba806b5da4365
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-05-24 11:34:43 +0200

    truthy and falsy

--- data-types/index.js
+++ data-types/index.js
@@ -48,3 +48,51 @@ var loc1 = findCarInLot(fiat2);
 var loc2 = findCarInLot(taxi);
 var loc3 = findCarInLot(chevy);
 var loc4 = findCarInLot(fiat1);
+
+//truthy and falsy
+function lieDetectorTest() {
+  var lies = 0;
+  var stolenDiamond = {};
+  if (stolenDiamond) {
+    console.log("You stole the diamond");
+    lies++;
+  }
+  var car = {
+    keysInPocket: null,
+  };
+  if (car.keysInPocket) {
+    console.log("Uh oh, guess you stole the car!");
+    lies++;
+  }
+  if (car.emptyGasTank) {
+    console.log("You drove the car after you stole it!");
+    lies++;
+  }
+  var foundYouAtTheCrimeScene = [];
+  if (foundYouAtTheCrimeScene) {
+    console.log("A sure sign of guilt");
+    lies++;
+  }
+  if (foundYouAtTheCrimeScene[0]) {
+    console.log("Caught with a stolen item!");
+    lies++;
+  }
+  var yourName = " ";
+  if (yourName) {
+    console.log("Guess you lied about your name");
+    lies++;
+  }
+  return lies;
+}
+var numberOfLies = lieDetectorTest();
+console.log("You told " + numberOfLies + " lies!");
+if (numberOfLies >= 3) {
+  console.log("Guilty as charged");
+}
+
+// var text = "YOU SHOULD NEVER SHOUT WHEN TYPING";
+// var presentableText = text.toLowerCase();
+// if (presentableText.length > 0) {
+//   alert(presentableText);
+// }
+
